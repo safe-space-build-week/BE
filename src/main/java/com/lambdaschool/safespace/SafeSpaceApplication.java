578594bc567493b6fe68adca0dc731327d0e5e28
danpatrickoneill <dan47573@gmail.com>
2019-05-20 17:25:49 -0400

    Added Application Context to main app file

--- src/main/java/com/lambdaschool/safespace/SafeSpaceApplication.java
+++ src/main/java/com/lambdaschool/safespace/SafeSpaceApplication.java
@@ -2,14 +2,22 @@ package com.lambdaschool.safespace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
+@EnableJpaAuditing
 @SpringBootApplication
 public class SafeSpaceApplication
 {
 
     public static void main(String[] args)
     {
-        SpringApplication.run(SafeSpaceApplication.class, args);
-    }
+        ApplicationContext ctx = SpringApplication.run(SafeSpaceApplication.class, args);
 
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+    }
 }
