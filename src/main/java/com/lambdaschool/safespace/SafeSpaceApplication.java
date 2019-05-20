db386402d52b6ae5db516242b1497c8947e7f637
danpatrickoneill <dan47573@gmail.com>
2019-05-20 15:30:29 -0400

    Missed files from initial commit

---
+++ src/main/java/com/lambdaschool/safespace/SafeSpaceApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.safespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeSpaceApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SafeSpaceApplication.class, args);
+    }
+
+}
